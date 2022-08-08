de8da700a112d64ef5aedf2fd7758099e93d164c
Mario J Maurello <marioj@purestake.com>
2022-08-08 19:18:27 +0200

    implementation of AWS AppConfig for feature flags - env variables not committed

--- pages/xcm.tsx
+++ pages/xcm.tsx
@@ -1,21 +1,42 @@
 import React from "react";
 import { Shell } from "../components/Shell";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { List } from "../components/XCM/List";
+import { List, Props as ListProps } from "../components/XCM/List";
+import { featureFlag } from "../utils/featureFlag";
 
 export async function getStaticProps({ locale }: { locale: string }) {
+  // @TODO ideally this could be done in a state management library so we avoid passing
+  // down props to the components
+  const assets = [
+    { id: 6, symbol: "KMA", name: "Calamari", flag: "disable_kma" },
+    { id: 7, symbol: "ASTR", name: "Astar", flag: "disable_astr" },
+    { id: 39, symbol: "LIT", name: "Litentry", flag: "disable_lit" },
+    { id: 56, symbol: "KINT", name: "interBTC", flag: "disable_kint" },
+    { id: 58, symbol: "BSX", name: "Basilisk", flag: "disable_bsx" },
+  ];
+
+  const hidratedAssets = await Promise.all(
+    assets.map(async (asset) => {
+      return {
+        ...asset,
+        disabled: await featureFlag(asset.flag),
+      };
+    })
+  );
+
   return {
     props: {
+      assets: hidratedAssets,
       ...(await serverSideTranslations(locale, ["navbar", "xcm"])),
       // Will be passed to the page component as props
     },
   };
 }
 
-export default function XCM() {
+export default function XCM({ assets }: ListProps) {
   return (
     <Shell>
-      <List />
+      <List assets={assets} />
     </Shell>
   );
 }
---
+++ utils/featureFlag.ts
@@ -0,0 +1,53 @@
+import {
+  AppConfigDataClient,
+  BadRequestException,
+  GetLatestConfigurationCommand,
+  StartConfigurationSessionCommand,
+} from "@aws-sdk/client-appconfigdata";
+
+const client = new AppConfigDataClient({});
+
+let existingToken: string;
+
+const getToken = async (): Promise<string> => {
+  const getSession = new StartConfigurationSessionCommand({
+    ApplicationIdentifier: process.env.APP_CONFIG_APP_IDENTIFIER,
+    ConfigurationProfileIdentifier:
+      process.env.APP_CONFIG_CONFIG_PROFILE_IDENTIFIER,
+    EnvironmentIdentifier: process.env.APP_CONFIG_ENVIRONMENT_IDENTIFIER,
+  });
+  const sessionToken = await client.send(getSession);
+  return sessionToken.InitialConfigurationToken || "";
+};
+
+export const featureFlag = async (flag: string): Promise<boolean> => {
+  if (!existingToken) {
+    existingToken = await getToken();
+    // console.log("existing token", existingToken);
+  }
+  try {
+    const command = new GetLatestConfigurationCommand({
+      ConfigurationToken: existingToken,
+    });
+    const response = await client.send(command);
+    let flags: any = {};
+    if (response.Configuration) {
+      let str = "";
+      for (let i = 0; i < response.Configuration.length; i++) {
+        str += String.fromCharCode(response.Configuration[i]);
+      }
+      const allFlag = JSON.parse(str);
+      flags = Object.assign({}, allFlag);
+    }
+    return Boolean(flags[flag]?.enabled);
+  } catch (err) {
+    if (err instanceof BadRequestException) {
+      existingToken = await getToken();
+      console.log(existingToken);
+      // recall
+      return featureFlag(flag);
+    } else {
+      throw err;
+    }
+  }
+};
